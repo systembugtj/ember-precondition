bd94091fb76143b3c6cbf95a9cd749df0cc33a97
Albert <albert.li@salesforce.com>
2016-10-10 11:20:26 -0700

    remove blueprints

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 /* jshint node: true */
 'use strict';
-var path = require("path");
 
 module.exports = {
-  name: 'ember-precondition',
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  }
+  name: 'ember-precondition'
 };
